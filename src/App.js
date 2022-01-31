16babcb0228ecd392abbaa2eae102fbaf51c6e86
Sushant <sushantpaudyal09@gmail.com>
2022-01-31 15:38:14 +0545

    version 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello this is flyrate.
+          Hello this is flyrate. Smile.
         </p>
         <a
           className="App-link"
